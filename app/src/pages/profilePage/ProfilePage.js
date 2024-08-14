c0f6c4f013909560698592a023bf0ae1e79195d2
alex-makaroff <github@amak.site>
2024-08-14 11:03:18 +0300

    изменен путь get запроса

--- app/src/pages/profilePage/ProfilePage.js
+++ app/src/pages/profilePage/ProfilePage.js
@@ -59,7 +59,7 @@ const ProfilePage = () => {
     console.log('userId', userId);
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/users/${userId}`)
+        axios.get(`http://89.223.126.50:5000/api/users/${userId}`)
             .then(response => {
                 setUser(response.data);
             })
